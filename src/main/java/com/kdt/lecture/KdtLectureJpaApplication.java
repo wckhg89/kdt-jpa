1f30da2652055771c2d81d7c405c2e23de6d760b
kanghonggu <gupp.kang@woowahan.com>
2021-08-21 11:44:01 +0900

    init commit

---
+++ src/main/java/com/kdt/lecture/KdtLectureJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtLectureJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtLectureJpaApplication.class, args);
+    }
+
+}
